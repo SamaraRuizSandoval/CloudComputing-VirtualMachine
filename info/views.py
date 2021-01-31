54e70944f026576f915f23d58e3ea122e67a758c
Samara Ruiz Sandoval <rsandoval.samara@gmail.com>
2021-01-30 23:17:03 -0500

    Deteling module-agents

--- info/views.py
+++ info/views.py
@@ -23,28 +23,28 @@ def index(request):
         remotePort = "Error"
 
     # Get information from user
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    # if x_forwarded_for:
+    #     ip = x_forwarded_for.split(',')[0]
+    # else:
+    #     ip = request.META.get('REMOTE_ADDR')
 
-    device_type = ""
-    browser_type = ""
-    browser_version = ""
-    os_type = ""
-    os_version = ""
-    if request.user_agent.is_mobile:
-        device_type = "Mobile"
-    if request.user_agent.is_tablet:
-        device_type = "Tablet"
-    if request.user_agent.is_pc:
-        device_type = "PC"
+    # device_type = ""
+    # browser_type = ""
+    # browser_version = ""
+    # os_type = ""
+    # os_version = ""
+    # if request.user_agent.is_mobile:
+    #     device_type = "Mobile"
+    # if request.user_agent.is_tablet:
+    #     device_type = "Tablet"
+    # if request.user_agent.is_pc:
+    #     device_type = "PC"
 
-    browser_type = request.user_agent.browser.family
-    browser_version = request.user_agent.browser.version_string
-    os_type = request.user_agent.os.family
-    os_version = request.user_agent.os.version_string
+    # browser_type = request.user_agent.browser.family
+    # browser_version = request.user_agent.browser.version_string
+    # os_type = request.user_agent.os.family
+    # os_version = request.user_agent.os.version_string
 
     context = {
         'IPAddr': IPAddr,
@@ -53,11 +53,11 @@ def index(request):
         'serverHost': hostname,
         'protocol': protocol,
         'userAgent': userAgent,
-        "device_type": device_type,
-        "browser_type": browser_type,
-        "browser_version": browser_version,
-        "os_type": os_type,
-        "os_version": os_version
+        # "device_type": device_type,
+        # "browser_type": browser_type,
+        # "browser_version": browser_version,
+        # "os_type": os_type,
+        # "os_version": os_version
     }
     return HttpResponse(template.render(context, request))
 
--- virtualMachine/settings.py
+++ virtualMachine/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_user_agents',
+    # 'django_user_agents',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_user_agents.middleware.UserAgentMiddleware',
+    # 'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'virtualMachine.urls'
